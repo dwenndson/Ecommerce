e52076d21deac80df85a30531ad0688224f0549b
diego wenndson <diego.wenndson@gmail.com>
2020-07-24 23:08:52 -0300

    Implementando interfaces e bases para o ecommerce, além do dominio e notificações

---
+++ EcommerceDDD/Domain/Services/ServiceProduto.cs
@@ -0,0 +1,47 @@
+﻿using Domain.Interfaces.InterfaceProduct;
+using Domain.Interfaces.InterfaceServices;
+using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Services
+{
+    public class ServiceProduto : IServiceProduct
+    {
+        private readonly IProduct _iproduct;
+        public ServiceProduto(IProduct product)
+        {
+            _iproduct = product;
+        }
+        public async Task AddProduct(Produto produto)
+        {
+            var validaNome = produto.ValidarPropriedadeString(produto.Nome, "Nome");
+
+            var validaValor = produto.ValidarPropriedadeDecimal(produto.Valor, "Valor");
+
+            if(validaNome && validaValor)
+            {
+                produto.Estado = true;
+                await _iproduct.Add(produto);
+
+            }
+        }
+
+        public async Task UpdateProduct(Produto produto)
+        {
+            var validaNome = produto.ValidarPropriedadeString(produto.Nome, "Nome");
+
+            var validaValor = produto.ValidarPropriedadeDecimal(produto.Valor, "Valor");
+
+            if (validaNome && validaValor)
+            {
+                produto.Estado = true;
+                await _iproduct.Update(produto);
+
+            }
+        }
+
+    }
+}
